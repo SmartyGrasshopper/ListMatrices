c4d76208cd06b75f39deefde096e8f7f0309065f
SmartyGrasshopper <69722322+SmartyGrasshopper@users.noreply.github.com>
2020-10-11 20:45:56 +0530

    Created this file
    Created the file with foundation class

---
+++ listmatrices.py
@@ -0,0 +1,8 @@
+''' Small lib to make matrices using python lists '''
+
+class Matrix:
+    def __init__(self, list, shape = (4,4)):
+        self.matrix = None
+    
+    def addMatrix(self):
+        pass
